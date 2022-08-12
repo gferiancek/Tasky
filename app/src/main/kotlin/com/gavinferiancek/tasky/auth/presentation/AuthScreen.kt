701dd2d35e2c548cc039e5acc538cc3a6e245d45
gferi <gferiancek@gmail.com>
2022-08-12 06:27:36 -0400

    Added Crossfade when switching layouts.

--- app/src/main/kotlin/com/gavinferiancek/tasky/auth/presentation/AuthScreen.kt
+++ app/src/main/kotlin/com/gavinferiancek/tasky/auth/presentation/AuthScreen.kt
@@ -1,5 +1,7 @@
 package com.gavinferiancek.tasky.auth.presentation
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
@@ -30,85 +32,111 @@ fun AuthScreen(
     events: (AuthEvents) -> Unit,
 ) {
     val spacing = LocalSpacing.current
-    CardLayout(
-        modifier = Modifier
-            .padding(top = spacing.large),
-        header = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.15f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-            ) {
-                Text(
-                    text = stringResource(
-                        id = if (state.showRegister) R.string.register_header else R.string.login_header
-                    ),
-                    style = MaterialTheme.typography.h2,
-                    color = contentColorFor(backgroundColor = MaterialTheme.colors.background),
-                )
-            }
-        },
-        footer = {
-            if (state.showRegister) {
-                Button(
-                    onClick = {
-                        events(AuthEvents.ResetState)
-                        events(AuthEvents.ToggleShowRegister(!state.showRegister))
-                    },
-                    shape = MaterialTheme.shapes.medium,
-                ) {
-                    Icon(
-                        modifier = Modifier
-                            .padding(
-                                top = spacing.small,
-                                bottom = spacing.small,
-                            ),
-                        painter = painterResource(id = R.drawable.ic_arrow_back_ios_new_24),
-                        contentDescription = null,
-                        tint = MaterialTheme.colors.onPrimary,
-                    )
-                }
-            } else {
-                Row(
+
+    Crossfade(
+        targetState = !state.showRegister,
+        animationSpec = tween(durationMillis = 600)
+    ) {
+        CardLayout(
+            modifier = Modifier
+                .padding(top = spacing.large),
+            header = {
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.15f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                 ) {
                     Text(
-                        text = stringResource(id = R.string.signup_base_text),
-                        style = MaterialTheme.typography.subtitle1,
-                        color = MaterialTheme.colors.muted
+                        text = stringResource(
+                            id = if (state.showRegister) R.string.register_header else R.string.login_header
+                        ),
+                        style = MaterialTheme.typography.h2,
+                        color = contentColorFor(backgroundColor = MaterialTheme.colors.background),
                     )
-                    ClickableText(
+                }
+            },
+            footer = {
+                if (state.showRegister) {
+                    Button(
                         onClick = {
                             events(AuthEvents.ResetState)
                             events(AuthEvents.ToggleShowRegister(!state.showRegister))
                         },
-                        text = AnnotatedString(
-                            text = stringResource(id = R.string.signup_clickable_text),
-                            spanStyle = SpanStyle(
-                                color = LightBlue,
-                            )
-                        ),
-                        style = MaterialTheme.typography.subtitle1,
-                    )
+                        shape = MaterialTheme.shapes.medium,
+                    ) {
+                        Icon(
+                            modifier = Modifier
+                                .padding(
+                                    top = spacing.small,
+                                    bottom = spacing.small,
+                                ),
+                            painter = painterResource(id = R.drawable.ic_arrow_back_ios_new_24),
+                            contentDescription = null,
+                            tint = MaterialTheme.colors.onPrimary,
+                        )
+                    }
+                } else {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.signup_base_text),
+                            style = MaterialTheme.typography.subtitle1,
+                            color = MaterialTheme.colors.muted
+                        )
+                        ClickableText(
+                            onClick = {
+                                events(AuthEvents.ResetState)
+                                events(AuthEvents.ToggleShowRegister(!state.showRegister))
+                            },
+                            text = AnnotatedString(
+                                text = stringResource(id = R.string.signup_clickable_text),
+                                spanStyle = SpanStyle(
+                                    color = LightBlue,
+                                )
+                            ),
+                            style = MaterialTheme.typography.subtitle1,
+                        )
+                    }
                 }
             }
-        }
-    ) {
-        Column {
-            if (state.showRegister) {
+        ) {
+            Column {
+                if (state.showRegister) {
+                    RoundedTextField(
+                        value = state.name,
+                        placeholder = stringResource(id = R.string.name_placeholder),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Text,
+                            imeAction = ImeAction.Next,
+                        ),
+                        trailingIcon = {
+                            if (state.isNameValidated) {
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colors.primaryVariant
+                                )
+                            }
+                        },
+                        onUpdateValue = { name ->
+                            events(AuthEvents.UpdateName(name))
+                        },
+                    )
+                }
                 RoundedTextField(
-                    value = state.name,
-                    placeholder = stringResource(id = R.string.name_placeholder),
+                    value = state.email,
+                    placeholder = stringResource(id = R.string.email_placeholder),
                     keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text,
+                        keyboardType = KeyboardType.Email,
                         imeAction = ImeAction.Next,
                     ),
                     trailingIcon = {
-                        if (state.isNameValidated) {
+                        if (state.isEmailValidated) {
                             Icon(
                                 imageVector = Icons.Default.Check,
                                 contentDescription = null,
@@ -116,76 +144,56 @@ fun AuthScreen(
                             )
                         }
                     },
-                    onUpdateValue = { name ->
-                        events(AuthEvents.UpdateName(name))
+                    onUpdateValue = { email ->
+                        events(AuthEvents.UpdateEmail(email))
                     },
                 )
-            }
-            RoundedTextField(
-                value = state.email,
-                placeholder = stringResource(id = R.string.email_placeholder),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Email,
-                    imeAction = ImeAction.Next,
-                ),
-                trailingIcon = {
-                    if (state.isEmailValidated) {
-                        Icon(
-                            imageVector = Icons.Default.Check,
-                            contentDescription = null,
-                            tint = MaterialTheme.colors.primaryVariant
-                        )
-                    }
-                },
-                onUpdateValue = { email ->
-                    events(AuthEvents.UpdateEmail(email))
-                },
-            )
-            RoundedTextField(
-                value = state.password,
-                visualTransformation = if (state.showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-                placeholder = stringResource(id = R.string.password_placeholder),
-                keyboardOptions = KeyboardOptions(
-                    keyboardType = KeyboardType.Password,
-                    imeAction = ImeAction.Done,
-                ),
-                trailingIcon = {
-                    IconButton(
-                        onClick = {
-                            events(AuthEvents.ToggleShowPassword(!state.showPassword))
-                        }
-                    ) {
-                        Icon(
-                            painter = painterResource(
-                                id = if (state.showPassword) R.drawable.ic_visibility_off_24 else R.drawable.ic_visibility_24
-                            ),
-                            contentDescription = stringResource(
-                                id = if (state.showPassword) R.string.hide_password_content_description else R.string.show_password_content_description
-                            ),
-                            tint = MaterialTheme.colors.muted,
-                        )
-                    }
-                },
-                onUpdateValue = { password ->
-                    events(AuthEvents.UpdatePassword(password))
-                },
-                onDone = {
-                    // TODO Trigger Login/Register Event
-                },
-            )
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                onClick = {
-                    // TODO Trigger Login/Register Event
-                },
-                shape = MaterialTheme.shapes.large
-            ) {
-                Text(
-                    text = stringResource(
-                        id = if (state.showRegister) R.string.register_button else R.string.login_button
+                RoundedTextField(
+                    value = state.password,
+                    visualTransformation = if (state.showPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                    placeholder = stringResource(id = R.string.password_placeholder),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done,
                     ),
+                    trailingIcon = {
+                        IconButton(
+                            onClick = {
+                                events(AuthEvents.ToggleShowPassword(!state.showPassword))
+                            }
+                        ) {
+                            Icon(
+                                painter = painterResource(
+                                    id = if (state.showPassword) R.drawable.ic_visibility_off_24 else R.drawable.ic_visibility_24
+                                ),
+                                contentDescription = stringResource(
+                                    id = if (state.showPassword) R.string.hide_password_content_description else R.string.show_password_content_description
+                                ),
+                                tint = MaterialTheme.colors.muted,
+                            )
+                        }
+                    },
+                    onUpdateValue = { password ->
+                        events(AuthEvents.UpdatePassword(password))
+                    },
+                    onDone = {
+                        // TODO Trigger Login/Register Event
+                    },
                 )
+                Button(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    onClick = {
+                        // TODO Trigger Login/Register Event
+                    },
+                    shape = MaterialTheme.shapes.large
+                ) {
+                    Text(
+                        text = stringResource(
+                            id = if (state.showRegister) R.string.register_button else R.string.login_button
+                        ),
+                    )
+                }
             }
         }
     }
